a5d1093e6803f3eb682419beda5f66d324073746
Matvey <mib15092005@gmail.com>
2024-09-13 19:37:59 +0300

    First anim

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("clearButton").onclick = function(e) {
+    // Если необходимо предотвратить/отменить событие по умолчанию,
+    // то необходимо вызвать метод preventDefault у события
+    // https://developer.mozilla.org/ru/docs/Web/API/Event/preventDefault
+    e.preventDefault();
+    // если необходимо также предотвратить дальнейшее "всплытие" события,
+    // то необходимо вызвать метод stopPropagation у события
+    // https://developer.mozilla.org/ru/docs/Web/API/Event/stopPropagation
+    // e.stopPropagation();
+    document.getElementById("textInput").value = "";
+}
